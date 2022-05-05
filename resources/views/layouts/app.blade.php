04ad3dbd5e2af87b3b1821f165866ea0996a2ceb
Luislo888 <b_yadof@hotmail.com>
2022-05-05 21:21:51 +0200

    Editar Comanda Ajax parece que Solucionado

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -106,7 +106,7 @@
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                         onclick="event.preventDefault();
-                                                                                                                                                                                                                                                                                 document.getElementById('logout-form').submit();">
+                                                                                                                                                                                                                                                                                     document.getElementById('logout-form').submit();">
                                         <i class="fa-solid fa-right-from-bracket"></i> {{ __('Cerrar Sesión') }}
                                     </a>
 
--- routes/web.php
+++ routes/web.php
@@ -51,6 +51,7 @@ Route::resource('/cocinero', CocineroController::class);
 Route::post('/comanda', [App\Http\Controllers\ComandaController::class, 'store'])->name('comanda');
 Route::get('/comanda/{id}', [App\Http\Controllers\ComandaController::class, 'show'])->name('comanda-edit');
 Route::patch('/comanda/{id}', [App\Http\Controllers\ComandaController::class, 'update'])->name('comanda-update');
+Route::post('/comanda/{id}', [App\Http\Controllers\ComandaController::class, 'update'])->name('comanda-update');
 
 
 // Route::resource('/comanda', ComandaController::class);
